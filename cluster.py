e09a133cdf6a32de53e66308234b56c4001cf703
Hackingni <rajavelu6927@gmail.com>
2024-04-08 22:15:14 +0530

    updated the programs

--- cluster.py
+++ cluster.py
@@ -1,1 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+data = pd.read_csv("D:\\Jeyashri\\IBM\\Datasets\\Country clusters.csv")
+data
+plt.scatter(data['Longitude'],data['Latitude'])
+plt.xlim(-180,180)
+plt.ylim(-90,90)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.show()
+x = data.iloc[:,1:3] 
+x
+kmeans = KMeans(3)
+kmeans.fit(x)
+identified_clusters = kmeans.fit_predict(x)
+identified_clusters
+data_with_clusters = data.copy()
+data_with_clusters['Clusters'] = identified_clusters 
+plt.scatter(data_with_clusters['Longitude'],data_with_clusters['Latitude'],c=data_with_clusters['Clusters'],cmap='rainbow')
 
